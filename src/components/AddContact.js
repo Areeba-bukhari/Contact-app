3d1322f89ba5c9ae98dd58b902036b41bc372ddc
unknown <ariibabukhari@gmail.com>
2023-08-06 21:05:23 +0100

    card

--- src/components/AddContact.js
+++ src/components/AddContact.js
@@ -1,18 +1,34 @@
 import React from "react";
 
 class AddContact extends React.Component {
-    render() {
-        return (
-            <div className="ui main">;
-                <h2>Add Contact</h2>
-                <form className="ui form">
-                    <div className="field">
-                         <label>Name</label>
-                         <input type="text" name="name" placeholder="Name" />
-                    </div>
-                    <div className="field">
-                         <label>Email</label>
-                         <input type="text" name="name" placeholder="Name" />
-                    </div>
-                    <button className="ui button blue">Add</button>
-                </form>
+    state  = {
+        name: "",
+        email: "",
+    };    
+}
+
+    add = (e) => {
+    e.preventDefault();
+    if (this.state.name === ""|| this.state.email === "") {
+
+        alert("ALl the fields are mandatory!");
+        return;
+    }    
+    this.props.addContactHandler(this.state);
+    this.setstate({ name: "*, email: "" });
+}; 
+render() {
+return
+‹div className= "ui main";
+<h2>Add Contact</h2>
+‹form className»"ul form" onSubmit=(this .add)>
+‹div className="field"
+(label Name‹/ label
+                    <input
+                        type="text"
+                        name="name"
+                        placeholder="Name"
+                        value={this.state.email}
+               </div>
+                <button className= " ui button blue">Add</button>
+                </div>
---
+++ src/components/ContactCard.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const CardContact = () => {
+
+     return(
+         <div className="item">
+         <div className= "content">
+          <div className="header">{contact.name}</div>
+          <div>{contact.email}</div>
+       </div>
+       <i className="trash alternate outline icon"></i>
+       </div>
+     );
+};
+
+export default ContactCard;
