0430c64386df7b74f817d07638b0dd2da99e584e
unknown <ariibabukhari@gmail.com>
2023-08-10 16:47:07 +0100

    used useeffect for localstorage in app,js file

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,21 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import Header from "./Header";
 import AddContact from "./AddContact";
 import ContactList from "./ContactList";
 
 function App() {
-  const [contents, setContacts] = useState([]);
+  const LOCAL_STORAGE_KEY = "contact";
+  const [contants, setContacts] = useState([]);
 
     const addContactHandeler = (contact) => {
       consoul.log(contact);
       setContacts([...contacts, contact])
     };
+
+    useEffect(() => {
+      localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(contact));
+    }, [contacts]);
     return (
     <div className="ui container">
      <Header />
